39cfb919846e04354f3ccd99f4e82f32310cf6ef
Ulysses Popple <Ulysses Popple>
2016-07-09 12:12:44 -0700

    Send time

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ import Control.Concurrent.Chan
 import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.Trans.Maybe
+import Data.Time.Clock
 import Sound.OSC
 import qualified Sound.OSC.Transport.FD as T
 import qualified Data.ByteString.Char8 as BS
@@ -15,8 +16,10 @@ type MessageQueue = Chan Message
 
 main = do
   conn <- openUDP "127.0.0.1" 9001
-  -- T.sendOSC conn $ Message "/shaders/0" [ASCII_String $ BS.pack "sine"]
-  keepSending conn sendSine
+  forkIO $ sync conn
+  keepSending conn sendBoth
+
+ostr = ASCII_String . BS.pack
 
 keepSending :: UDP -> ReaderT UDP IO () -> IO ()
 keepSending t r = do
@@ -25,9 +28,31 @@ keepSending t r = do
   print "next"
   keepSending t r
 
+sync :: UDP -> IO ()
+sync t = do
+  runReaderT sendTime t
+  threadDelay 16000
+  sync t
+
+sendBoth :: (T.Transport t) => ReaderT t IO ()
+sendBoth = do
+  _ <- sendSine
+  liftIO $ threadDelay 1000000
+  -- _ <- sendLine
+  return ()
+
 sendSine :: (T.Transport t) => ReaderT t IO ()
-sendSine = do
-  let msg =  Message "/shaders/0" [ASCII_String $ BS.pack "sine"]
-  return $ print msg
-  return $ print "hi"
-  sendOSC msg
+sendSine =
+  sendOSC $ Message "/shader" [ostr "sine"]
+
+sendLine :: (T.Transport t) => ReaderT t IO ()
+sendLine =
+  sendOSC $ Message "/shader" [ostr "line_down"]
+
+sendTime :: (T.Transport t) => ReaderT t IO ()
+sendTime = do
+  now <- liftIO getCurrentTime
+  sendOSC $ Message "/shader/uniform/time" [float . realToFrac $ utctDayTime now]
+
+
+
