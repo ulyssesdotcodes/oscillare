6fe70257982d299e1fdeead6b953df2b174f7d14
Ulysses Popple <ulysses.popple@gmail.com>
2016-07-13 10:08:15 -0700

    Start on pattern

--- app/Main.hs
+++ app/Main.hs
@@ -12,56 +12,19 @@ import Sound.OSC
 import qualified Sound.OSC.Transport.FD as T
 import qualified Data.ByteString.Char8 as BS
 
-
-type MessageQueue = Chan Message
+data Pattern a = Pattern { arc :: Float -> [a] }
 
 ostr = ASCII_String . BS.pack
 
 main = do
   conn <- openUDP "127.0.0.1" 9001
-  forkIO $ runReaderT (sync []) conn
-  flip runReaderT conn $ do
-    _ <- sendScale
-    sendBoth
-
-sync :: (T.Transport t) => [String] -> ReaderT t IO ()
-sync addrs = do
-  now <- liftIO getCurrentTime
-  let floatNow = realToFrac $ utctDayTime now
-  mapM_ (\a -> sendOSC $ Message "/progs/uniform" [ostr a, ostr "time", float $ mod' 1.0 floatNow]) addrs
-  sendOSC $ Message "/shader/uniform/time" [float floatNow]
-  liftIO $ threadDelay 16000
-  sync addrs
-
-sendBoth :: (T.Transport t) => ReaderT t IO ()
-sendBoth = do
-  sendSine
-  liftIO $ threadDelay 1000000
-  sendLine
-  liftIO $ threadDelay 1000000
-  sendBoth
-
-sendSine :: (T.Transport t) => ReaderT t IO ()
-sendSine = do
-  sendOSC $ Message "/progs" [ostr "p1", ostr "sine"]
-
-sendLine :: (T.Transport t) => ReaderT t IO ()
-sendLine =
-  sendOSC $ Message "/progs" [ostr "p1", ostr "line_down"]
-
-sendScale :: (T.Transport t) => ReaderT t IO ()
-sendScale = do
-  sendOSC $ Message "/shader" [ostr "scale"]
-  sendOSC $ Message "/shader/base" [ostr "p1"]
-
-sendTime :: (T.Transport t) => ReaderT t IO ()
-sendTime = do
-  now <- liftIO getCurrentTime
-  let floatNow = realToFrac $ utctDayTime now
-  sendOSC $ Message "/shader/uniform/time" [float floatNow]
-  sendOSC $ Message "/progs/uniform" [ostr "p1", ostr "time", float $ mod' 1.0 floatNow ]
-  sendOSC $ Message "/progs/uniform" [ostr "p1", ostr "scale", float . sin $ mod' 1.0 floatNow]
+  flip runReaderT conn $ sendOSC $ Message "/connection" []
 
 
+instance Functor Pattern where
+  fmap f (Pattern a) = Pattern (\t -> f <$> a t)
 
+instance Applicative Pattern where
+  pure p = Pattern $ const [p]
+  Pattern fs <*> Pattern ms = Pattern (\t -> fs t <*> ms t)
 
