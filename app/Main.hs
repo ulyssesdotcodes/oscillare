cd4d5a61bb67de2329eddcd32b5847dc2338d4dd
Ulysses Popple <Ulysses Popple>
2016-07-13 19:07:35 -0700

    Messages from time working

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Control.Concurrent
@@ -7,14 +8,16 @@ import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.Trans.Maybe
 import Data.Fixed
+import Data.Text
+import Data.Text.Encoding
 import Data.Time.Clock
 import Sound.OSC
-import qualified Sound.OSC.Transport.FD as T
 import qualified Data.ByteString.Char8 as BS
+import qualified Sound.OSC.Transport.FD as T
 
-data Pattern a = Pattern { arc :: Float -> [a] }
+data Pattern a = Pattern { arc :: Int -> [a] } -- From 0-128
 
-ostr = ASCII_String . BS.pack
+ostr = ASCII_String . encodeUtf8
 
 main = do
   conn <- openUDP "127.0.0.1" 9001
@@ -28,3 +31,41 @@ instance Applicative Pattern where
   pure p = Pattern $ const [p]
   Pattern fs <*> Pattern ms = Pattern (\t -> fs t <*> ms t)
 
+perCycle :: Int -> Pattern a -> Pattern a
+perCycle times (Pattern pf) = Pattern timeF
+  where
+    timeFrac = quot 128 times
+    timeF t =
+      if (t `mod` timeFrac) == 0 then pf t else []
+
+timePattern :: Pattern Float
+timePattern = Pattern $ (:[]) . (/ (128::Float)) . fromIntegral
+
+data UniformType = UniformFloat Float
+
+data Uniform a =  Uniform { name :: Text, value :: a }
+
+timeUniform :: Pattern (Uniform Float)
+timeUniform = uniformPattern "time" timePattern
+
+uniformPattern :: Text -> Pattern Float -> Pattern (Uniform Float)
+uniformPattern n = fmap (Uniform n)
+
+data Program
+  = Sine
+  | Line
+  | Scale
+
+programText :: Program -> Text
+programText Sine = "sine"
+programText Line = "line"
+programText Scale = "scale"
+
+progName :: Text -> Program -> [Pattern (Uniform Float)] -> [Pattern Message]
+progName n p us = progMsg:uMsgs
+  where
+    progMsg = perCycle 1 <$> pure $ Message "/progs" [ostr n, ostr $ programText p]
+    uMsgs = (uniformMessage n <$>) <$> us
+
+uniformMessage :: Text -> Uniform Float -> Message
+uniformMessage n u = Message "/progs/uniform" [ostr n, ostr $ name u, float $ value u]
