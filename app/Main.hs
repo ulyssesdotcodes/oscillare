5d407ffb2d5e8ca2ca27612d4517b52a201d718f
Ulysses Popple <ulysses.popple@gmail.com>
2016-07-14 20:49:34 -0700

    Working synced

--- app/Main.hs
+++ app/Main.hs
@@ -16,15 +16,21 @@ import System.Environment
 import qualified Data.ByteString.Char8 as BS
 import qualified Sound.OSC.Transport.FD as T
 
-data Pattern a = Pattern { arc :: Int -> [a] } -- From 0-128
+data Pattern a = Pattern { arc :: Float -> [a] }
 
 ostr = ASCII_String . encodeUtf8
 
 main = do
   now <- getCurrentTime
-  conn <- openUDP "127.0.0.1" 3333
-  mvarT <- newMVar $ TempoState conn (progName (pack "p1") Sine [timeUniform, perCycle 2 $ uniformPattern (pack "scale") (sin <$> (* 3.1415) <$> timePattern)]) (utctDayTime now) (secondsToDiffTime 1) 0
+  conn <- openUDP "127.0.0.1" 9001
+  mvarT <- newMVar $ TempoState conn (mappend sineProg scaleEffect) (utctDayTime now) (secondsToDiffTime 1) 0
   sync mvarT
+
+sineProg :: Pattern Message
+sineProg = programMessage (pack "p1") (progName Sine) [timeUniform]
+
+scaleEffect :: Pattern Message
+scaleEffect = effectMessage (pack "s") Scale (pack "p1") [uniformPattern "scale" ((* 0.5) . sin . (* (3.1415 * 2)) <$> timePattern)]
 
 instance Functor Pattern where
   fmap f (Pattern a) = Pattern (\t -> f <$> a t)
@@ -55,10 +61,10 @@ perCycle times (Pattern pf) = Pattern timeF
   where
     timeFrac = quot 128 times
     timeF t =
-      if (t `mod` timeFrac) == 0 then pf t else []
+      if (round (t * 128) `mod` timeFrac) == 0 then pf t else []
 
 timePattern :: Pattern Float
-timePattern = Pattern $ (:[]) . (/ (128::Float)) . fromIntegral
+timePattern = Pattern $ (:[])
 
 data UniformType = UniformFloat Float
 
@@ -73,23 +79,41 @@ uniformPattern n = fmap (Uniform n)
 data Program
   = Sine
   | Line
-  | Scale
+
+data Effect
+  = Scale
 
 programText :: Program -> Text
 programText Sine = "sine"
 programText Line = "line"
-programText Scale = "scale"
 
-progName :: Text -> Program -> [Pattern (Uniform Float)] -> Pattern Message
-progName n p us = mappend progMsg $ mconcat uMsgs
+effectText :: Effect -> Text
+effectText Scale = "scale"
+
+data ProgramName a = ProgramName { prog :: a, nameF :: (a -> Text) }
+
+progName :: Program -> ProgramName Program
+progName t = ProgramName t programText
+
+effectName :: Effect -> ProgramName Effect
+effectName t = ProgramName t effectText
+
+programMessage :: Text -> ProgramName a -> [Pattern (Uniform Float)] -> Pattern Message
+programMessage n p us = mappend progMsg $ mconcat uMsgs
   where
-    progMsg = perCycle 1 <$> pure $ Message "/progs" [ostr n, ostr $ programText p]
+    progMsg = perCycle 1 <$> pure $ Message "/progs" [ostr n, ostr $ nameF p (prog p)]
     uMsgs = (uniformMessage n <$>) <$> us
+
+effectMessage :: Text -> Effect -> Text -> [Pattern (Uniform Float)] -> Pattern Message
+effectMessage n e b us = mappend progMsg baseMsg
+  where
+    progMsg = programMessage n (effectName e) us
+    baseMsg = perCycle 1 <$> pure $ Message "/progs/base" [ostr n, ostr b]
 
 uniformMessage :: Text -> Uniform Float -> Message
 uniformMessage n u = Message "/progs/uniform" [ostr n, ostr $ name u, float $ value u]
 
-data TempoState = TempoState { conn :: UDP, pattern :: Pattern Message, start :: DiffTime, cycleLength :: DiffTime, current :: Int }
+data TempoState = TempoState { conn :: UDP, pattern :: Pattern Message, start :: DiffTime, cycleLength :: DiffTime, current :: Float }
 
 updateCycle :: MonadIO io => DiffTime -> StateT TempoState io ()
 updateCycle now = do
@@ -98,12 +122,12 @@ updateCycle now = do
   if diffTime > cycleLength tState then
     put $ tState { start = now, current = 0 }
   else
-    put $ tState { current = round (diffTime * (fromIntegral 128)) }
+    put $ tState { current = (fromRational . toRational) (diffTime / cycleLength tState) }
 
 sendMessages :: MonadIO io => StateT TempoState io ()
 sendMessages = do
   tState <- get
-  -- liftIO $ T.sendOSC (conn tState) $ Bundle 0 $ arc (pattern tState) (current tState)
+  liftIO $ T.sendOSC (conn tState) $ Bundle 0 $ arc (pattern tState) (current tState)
   liftIO $ print $ arc (pattern tState) (current tState)
 
 frame :: MonadIO io => StateT TempoState io ()
